871d0148bf9c5c6aad753c61e58f2bc7d5e47440
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-09-02 21:08:00 +0200

    Fix small errors

--- static/build/service-worker.js
+++ static/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6daae1a8732f6d4dd90ebefa38b856d1.js"
+  "/precache-manifest.72616012f01187af4ab9cc7b9361dcf4.js"
 );
 
 self.addEventListener('message', (event) => {
