671a59cff6b0c423f9f2fb70837d214bfa9b89ae
bonczol <filip.bonczyk@student.put.poznan.pl>
2020-09-09 20:53:29 +0200

    Autofocus on search input, searcher closing on Esc press

--- client/art_gallery/src/searching/Searcher.js
+++ client/art_gallery/src/searching/Searcher.js
@@ -12,23 +12,29 @@ export default class Searcher extends React.Component {
 		};
 
 		this.handleSearchPhraseChange = this.handleSearchPhraseChange.bind(this);
+		this.handleEscKeyDown = this.handleEscKeyDown.bind(this); 
 	}
 
-
 	handleSearchPhraseChange(event) {
-		this.setState({phrase: event.target.value});
+		this.setState({ phrase: event.target.value });
+	}
+
+	handleEscKeyDown(event) {
+ 		if (event.keyCode == 27 /*esc*/) {
+			this.props.onStopSearching();
+		}
 	}
 
 	render() {
 		return (
-			<div className="Searcher">
-                <div className="SearcherInsides">
-                    <IconButton onClick={this.props.onStopSearching} className="Searcher-close">
-                        <MdClose className="Searcher-close-icon"/>
-                    </IconButton>
-                    <input className="Searcher-searchbar" type="search" placeholder="Search" onChange={this.handleSearchPhraseChange}/>
-                    <SearchResults searchPhrase={this.state.phrase} onLinkClick={this.props.onStopSearching}/>
-                </div>
+			<div className="Searcher" onKeyDown={this.handleEscKeyDown}>
+				<div className="SearcherInsides">
+					<IconButton onClick={this.props.onStopSearching} className="Searcher-close">
+						<MdClose className="Searcher-close-icon" />
+					</IconButton>
+					<input className="Searcher-searchbar" type="search" placeholder="Search" autoFocus onKeyDown={this.handleEscKeyDown} onChange={this.handleSearchPhraseChange} />
+					<SearchResults searchPhrase={this.state.phrase} onLinkClick={this.props.onStopSearching} />
+				</div>
 			</div>
 		);
 	}
