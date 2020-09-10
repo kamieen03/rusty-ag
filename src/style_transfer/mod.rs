7b65e6cf5f39f7e23dd28260d05fd068a40b5f50
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-09-10 20:01:45 +0200

    Update style transfer

--- src/style_transfer/mod.rs
+++ src/style_transfer/mod.rs
@@ -4,7 +4,9 @@ use tch::{CModule, vision::image, Tensor};
 use rocket::response::Redirect;
 use rocket_contrib::json::Json;
 use serde::{Deserialize, Serialize};
-
+use std::io;
+use tempfile::{Builder, NamedTempFile};
+use std::ffi::OsStr;
 
 
 lazy_static! {
@@ -13,12 +15,36 @@ lazy_static! {
     static ref DECODER: CModule = tch::CModule::load("src/style_transfer/decoder.pt").unwrap();
 }
 
-pub fn transform_() {
-    let content = image::load("src/style_transfer/chicago.png").unwrap()
+fn fetch_image(url: String) -> NamedTempFile {
+    let mut resp = reqwest::blocking::get(&url).expect("request failed");
+    let sname = url.as_str()
+                   .rsplit('/')
+                   .next()
+                   .unwrap();
+    let mut out = Builder::new()
+            .prefix(sname)
+            .suffix(".jpg")
+            .tempfile()
+            .unwrap();
+    io::copy(&mut resp, &mut out).expect("failed to copy content");
+    out
+}
+
+pub fn transform_(content: Vec<u8>, shape: [i64; 2], style_url: String) -> String{
+    //let content = image::load("src/style_transfer/chicago.png").unwrap()
+   let content = Tensor::of_slice(content.as_slice())
+                    .reshape(&[shape[0], shape[1], 3])
                     .unsqueeze(0)
                     .to_kind(tch::Kind::Float) / 255.;
     let (_n, _c, ch, cw) = content.size4().unwrap();
-    let mut style = image::load("src/style_transfer/style.jpg")
+
+    let style_file = fetch_image(style_url);
+    let sname = style_file
+        .path()
+        .file_name()
+        .and_then(OsStr::to_str)
+        .unwrap();
+    let mut style = image::load(sname)
                     .unwrap();
     style = image::resize(&style, cw, ch)
                     .unwrap()
@@ -30,27 +56,32 @@ pub fn transform_() {
     let mut image = Tensor::cat(&[c_f,s_f], 0);
     image = MATRIX.forward_ts(&[image]).unwrap();
     image = DECODER.forward_ts(&[image]).unwrap().squeeze();
-
     image = image.clamp(0,1) * 255.;
     image = image.to_kind(tch::Kind::Uint8);
-    image::save(&image, "output.jpg").unwrap();
+
+    let hash = md5::compute(sname.to_string().into_bytes());
+    let fname = format!("{:x}.jpg", hash);
+    image::save(&image, &fname).unwrap();
+    fname
 }
 
 #[derive(Serialize, Deserialize)]
 #[allow(non_snake_case)]
 pub struct Input {
-    file: Vec<u8>,
+    file: String,
     artworkId: String,
 }
 
 #[post("/transform", data = "<input>")]
 pub fn transform(input: Json<Input>) -> Redirect {
-    let hash = "2137";
-    Redirect::to(uri!(get_transformed_image: hash))
+    //let fname = transform_(input.file, input.shape, input.artworkId);
+    Redirect::to(uri!(get_transformed_image: "xd"))
 }
 
-#[get("/result/<hash>")]
-pub fn get_transformed_image(hash: String) -> String {
+#[get("/result/<fname>")]
+pub fn get_transformed_image(fname: String) -> String {
     "OK".to_string()
+    //Option<NamedFile> {
+    //NamedFile::open(Path::new("static/build/index.html")).ok()
 }
 
