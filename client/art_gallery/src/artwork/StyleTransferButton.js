bfbecd9295e7ffa0bea35262ea4af3c261ca3456
bonczol <filip.bonczyk@student.put.poznan.pl>
2020-09-10 21:15:52 +0200

    Sending size of an image

--- client/art_gallery/src/artwork/StyleTransferButton.js
+++ client/art_gallery/src/artwork/StyleTransferButton.js
@@ -4,22 +4,33 @@ import { POST_STYLE_TRANSFER } from './../constants.js'
 import { toBase64 } from './../helper.js'
 
 
-export default function StyleTransferButton(props) {
+export default function StyleTransferButton(props) { 
+
+    function loadImage(src){
+        return new Promise((resolve, reject) => {
+          let img = new Image();
+          img.onload = () => resolve(img);
+          img.onerror = reject;
+          img.src = src;
+        })
+      }
 
     async function handleUpload(event) {
         const file = event.target.files[0];
         const fileBase64 = await toBase64(file);
+        const img = await loadImage(fileBase64);
 
         const data = {
             artworkId: props.artworkId,
-            file: fileBase64
+            file: fileBase64,
+            width: img.width,
+            height: img.height
         }
 
         const response = await fetch(POST_STYLE_TRANSFER, {
             method: "POST",
             body: JSON.stringify(data)
         })
-        console.log(response);
     }
 
     return (
