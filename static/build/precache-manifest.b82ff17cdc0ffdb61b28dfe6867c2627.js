8c245967dd36fc631c9ea27d5dbc81863aab368a
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-09-10 17:15:47 +0200

    Update front

---
+++ static/build/precache-manifest.b82ff17cdc0ffdb61b28dfe6867c2627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630c642611414698e4fc863b46ac025d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adcab47b62f2add66eb6",
+    "url": "/static/css/main.7f9c532c.chunk.css"
+  },
+  {
+    "revision": "12bf21af5eff0ab1721b",
+    "url": "/static/js/2.e0b3f341.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e0b3f341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adcab47b62f2add66eb6",
+    "url": "/static/js/main.0583f4c4.chunk.js"
+  },
+  {
+    "revision": "ad10369898f1878be166",
+    "url": "/static/js/runtime-main.774bb9a8.js"
+  }
+]);
