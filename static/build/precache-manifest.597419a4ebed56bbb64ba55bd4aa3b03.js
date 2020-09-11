d15b2e011297febf40f908e8bd91959c2df1ff3e
Kamil Burdziński <kamil.burdzinski@student.put.poznan.pl>
2020-09-11 19:30:55 +0200

    Update react build

---
+++ static/build/precache-manifest.597419a4ebed56bbb64ba55bd4aa3b03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffda2c86b617fd5ec5d4d4ae1d86270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c74edaeef119a4ca618",
+    "url": "/static/css/main.6f91a1f2.chunk.css"
+  },
+  {
+    "revision": "071120e63de5a7e83dcd",
+    "url": "/static/js/2.b527b8d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b527b8d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c74edaeef119a4ca618",
+    "url": "/static/js/main.b474d74f.chunk.js"
+  },
+  {
+    "revision": "ad10369898f1878be166",
+    "url": "/static/js/runtime-main.774bb9a8.js"
+  }
+]);
